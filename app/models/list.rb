540355d5bd0295a92612485a3d7e0b6176d93049
Luis Felipe Jimenez Roldan <luisfelipejimenezroldan@gmail.com>
2020-09-25 11:45:41 -0300

    progresando con formularios

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,7 @@
 class List < ApplicationRecord
+    has_many :items
+    validates_presence_of :name, message: 'debes escribir un nombre'
+    validates :name, format: {with: /\D/, message: 'solo puede llevar letras'}
+    validates :name, length: {minimum: 3, message: 'debe tener un mínimo de 3 caracteres'}
+    validates :name, length: {maximum: 30, message: 'debe tener un máximo de 30 caracteres'}
 end
